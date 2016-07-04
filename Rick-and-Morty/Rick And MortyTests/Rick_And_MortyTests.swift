2905957d9113849a3359668a13c3afcc58ae30f0
Wagner Truppel <wagner@novoda.com>
2016-07-04 16:50:28 +0200

    Adds Giuseppe's 'Rick-and-Morty' pairing project.

---
+++ Rick-and-Morty/Rick And MortyTests/Rick_And_MortyTests.swift
@@ -0,0 +1,16 @@
+//
+//  Rick_And_MortyTests.swift
+//  Rick And MortyTests
+//
+//  Created by Giuseppe Basile on 01/06/2016.
+//  Copyright © 2016 Novoda. All rights reserved.
+//
+
+import XCTest
+@testable import Rick_And_Morty
+
+class Rick_And_MortyTests: XCTestCase {
+    func testExample() {
+        XCTAssert(false)
+    }
+}
