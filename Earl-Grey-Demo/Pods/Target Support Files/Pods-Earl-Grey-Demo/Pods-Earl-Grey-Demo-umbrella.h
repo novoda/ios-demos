fa6dcbd35a9fe3e10f1c37695858cd9d62776a1e
amlcurran <aml.curran@gmail.com>
2017-02-15 16:04:13 +0000

    Initial commit for Earl Grey demo

---
+++ Earl-Grey-Demo/Pods/Target Support Files/Pods-Earl-Grey-Demo/Pods-Earl-Grey-Demo-umbrella.h
@@ -0,0 +1,8 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#endif
+
+
+FOUNDATION_EXPORT double Pods_Earl_Grey_DemoVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_Earl_Grey_DemoVersionString[];
+
---
+++ Earl-Grey-Demo/Pods/Target Support Files/Pods-Earl-Grey-DemoTests/Pods-Earl-Grey-DemoTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Earl_Grey_DemoTests : NSObject
+@end
+@implementation PodsDummy_Pods_Earl_Grey_DemoTests
+@end
