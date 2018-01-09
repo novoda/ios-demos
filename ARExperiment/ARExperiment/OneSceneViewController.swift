a611443a1b78ba4ee9cf327e2af40967e3c6a0f9
Berta Devant <berta@novoda.com>
2018-01-09 18:50:28 +0100

    Add AR to the project

--- ARExperiment/ARExperiment/OneSceneViewController.swift
+++ ARExperiment/ARExperiment/OneSceneViewController.swift
@@ -105,10 +105,3 @@ extension OneSceneViewController: ARSCNViewDelegate {
         }
     }
 }
-
-extension float4x4 {
-    var translation: float3 {
-        let translation = self.columns.3
-        return float3(translation.x, translation.y, translation.z)
-    }
-}
--- ARExperiment/ARExperiment/RecognizeObjectsViewController.swift
+++ ARExperiment/ARExperiment/RecognizeObjectsViewController.swift
@@ -13,14 +13,37 @@ import SceneKit
 class RecognizeObjectsViewController: UIViewController {
 
     @IBOutlet weak var sceneView: ARSCNView!
+    fileprivate var nodeModel: SCNNode?
     fileprivate let dispatchQueueML = DispatchQueue(label: "com.hw.dispatchqueueml") // A Serial Queue
     fileprivate let yolo = YOLO()
     fileprivate var boundingBox = BoundingBox()
 
+    //MARK: These strings is what you need to switch between different 3D objects
+    /** NodeName is the name of the object you want to show, not necessarily the name of the file.
+     - You can find the nodeName and change when opening the file on SceneKit Editor (click on the file or right click and use open as SceneKit Editor)
+     - on the left bottom side of the corner there should be an icon called "Show the scene graph View" click on that, you will now see the hierarchy of the object, tap the object at the top you want to use
+     - on the right top of xcode there should be a button called "Hide or show utilities" open the utilities using it
+     - On the top of the utilities look for the cube icon called "Show the nodes inspector" and click on that
+     - Under identity -> Name there should be a textField, that is the nodeName you need for here
+     **/
+    private let nodeName = "icecream"
+    private let fileName = "icecream"
+    private let fileExtension = "dae"
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        // Set the view's delegate
+        sceneView.delegate = self
+
+        // Show statistics such as fps and timing information
+        sceneView.showsStatistics = true
+        sceneView.debugOptions = [ARSCNDebugOptions.showFeaturePoints, ARSCNDebugOptions.showWorldOrigin]
+
+        nodeModel = createSceneNodeForAsset(nodeName, assetPath: "art.scnassets/\(fileName).\(fileExtension)")
+
+        // Begin Loop to Update CoreML
+        loopCoreMLUpdate()
     }
 
     override func didReceiveMemoryWarning() {
@@ -28,6 +51,17 @@ class RecognizeObjectsViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    //MARK: ARKit functions
+
+    private func createSceneNodeForAsset(_ assetName: String, assetPath: String) -> SCNNode? {
+        guard let paperPlaneScene = SCNScene(named: assetPath) else {
+            return nil
+        }
+        let carNode = paperPlaneScene.rootNode.childNode(withName: assetName, recursively: true)
+        return carNode
+    }
+
+    //MARK: Core ML Yolo functions
     private func loopCoreMLUpdate() {
         dispatchQueueML.async { [weak self] in
             guard let strongSelf = self else {
@@ -58,5 +92,22 @@ class RecognizeObjectsViewController: UIViewController {
     func show(prediction: YOLO.Prediction) {
         boundingBox.show(frame: prediction.rect, label: "", color: .blue)
     }
+}
 
+extension RecognizeObjectsViewController: ARSCNViewDelegate {
+
+    func renderer(_ renderer: SCNSceneRenderer, didAdd node: SCNNode, for anchor: ARAnchor) {
+        if !anchor.isKind(of: ARPlaneAnchor.self) {
+            DispatchQueue.main.async {
+                guard let model = self.nodeModel else {
+                    print("we have no model")
+                    return
+                }
+                let modelClone = model.clone()
+                modelClone.position = SCNVector3Zero
+                // Add model as a child of the node
+                node.addChildNode(modelClone)
+            }
+        }
+    }
 }
