fa7503cd0dca18eb25e63355404fabbb6bdd4a88
Wayne Rumble <rumblewayne@gmail.com>
2018-01-12 11:18:13 +0000

    Create ModelFactory

---
+++ ARDemos/ARDemos/ModelFactory.swift
@@ -0,0 +1,35 @@
+import Foundation
+
+struct ResponseData: Decodable {
+    var model: [Model]
+}
+
+struct Model: Decodable {
+    let filePath: String
+    let fileName: String
+    let fileExtension: String
+    let nodes: [Node]
+}
+
+struct Node: Decodable {
+    let name: String
+    let type: String
+}
+
+class ModelFactory {
+
+    func parseJSON() -> [Model]? {
+        if let url = Bundle.main.url(forResource: "ModelsData", withExtension: "json") {
+            do {
+                let data = try Data(contentsOf: url)
+                let decoder = JSONDecoder()
+                let jsonData = try decoder.decode(ResponseData.self, from: data)
+                
+                return jsonData.model
+            } catch {
+                print("error:\(error)")
+            }
+        }
+        return nil
+    }
+}
