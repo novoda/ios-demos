6bd37148d6919bc0e6e35a3ecb288e6a265b75a1
Wayne Rumble <rumblewayne@gmail.com>
2018-01-12 11:18:55 +0000

    Update ViewController

--- ARDemos/ARDemos/ViewController.swift
+++ ARDemos/ARDemos/ViewController.swift
@@ -5,21 +5,27 @@ class ViewController: UIViewController {
 
     @IBOutlet var sceneView: ARView!
     @IBOutlet var bottomBar: BottomBar!
-    private let viewModel = ObjectModel()
+    
+    private var modelNodeModel: SCNNode?
+    private var lightNodeModel: SCNNode?
+    private var planeNodeModel: SCNNode?
+    
+    private var models: [Model]!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         // Set the view's delegate
         sceneView.delegate = self
-        viewModel.getModels()
     }
+
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         
         // Create a session configuration
         let configuration = ARWorldTrackingConfiguration()
+        configuration.isLightEstimationEnabled = true;
 
         // Run the view's session
         sceneView.session.run(configuration)
@@ -34,13 +40,36 @@ class ViewController: UIViewController {
 }
 
 extension ViewController: ARSCNViewDelegate {
-    /*
-     // Override to create and configure nodes for anchors added to the view's session.
-     func renderer(_ renderer: SCNSceneRenderer, nodeFor anchor: ARAnchor) -> SCNNode? {
-     let node = SCNNode()
-
-     return node
-     }
-     */
-
+    
+    func renderer(_ renderer: SCNSceneRenderer, didAdd node: SCNNode, for anchor: ARAnchor) {
+        if !anchor.isKind(of: ARPlaneAnchor.self) {
+            DispatchQueue.main.async { [weak self] in
+                guard let strongSelf = self else { return }
+                guard let model = strongSelf.modelNodeModel else {
+                    print("we have no model")
+                    return
+                }
+                
+                let modelClone = model.clone()
+                modelClone.position = SCNVector3Zero
+                
+                node.addChildNode(modelClone)
+                node.addChildNode(strongSelf.lightNodeModel!)
+                node.addChildNode(strongSelf.planeNodeModel!)
+                                
+                strongSelf.addLightEstimate()
+            }
+        }
+    }
+    
+    private func addLightEstimate() {
+        sceneView.autoenablesDefaultLighting = false;
+        
+        let estimate: ARLightEstimate!
+        estimate = self.sceneView.session.currentFrame?.lightEstimate
+        
+        let light: SCNLight!
+        light = self.lightNodeModel?.light
+        light.intensity = estimate.ambientIntensity
+    }
 }
