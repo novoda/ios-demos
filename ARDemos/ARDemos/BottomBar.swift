27a6a733173799481d5f4c7e3b0b630a044ffb5f
Wayne Rumble <rumblewayne@gmail.com>
2018-01-12 11:17:11 +0000

    Setup BottomBar

--- ARDemos/ARDemos/BottomBar.swift
+++ ARDemos/ARDemos/BottomBar.swift
@@ -3,26 +3,59 @@ import UIKit
 class BottomBar: UIView {
 
     private let scrollView = UIScrollView()
+    private var models: [Model]!
 
     required init?(coder aDecoder: NSCoder) {
-        <#code#>
+        super.init(coder: aDecoder)
     }
 
-    override init(frame: CGRect) {
+    init(frame: CGRect, models: [Model]) {
+        super.init(frame: frame)
+        self.models = models
+        getModels()
         setUpViews()
         setUpLayout()
     }
+    
+    private func getModels() {
+        let modelFactory = ModelFactory()
+        models = modelFactory.parseJSON()
+    }
 
     private func setUpViews() {
-
+        addModelButtons()
         addSubview(scrollView)
     }
 
     private func setUpLayout() {
-
+        applyConstraint(for: .top)
+        applyConstraint(for: .right)
+        applyConstraint(for: .bottom)
+        applyConstraint(for: .left)
+    }
+    
+    private func applyConstraint(for attribute: NSLayoutAttribute) {
+        let constraint = NSLayoutConstraint(item: scrollView,
+                                  attribute: attribute,
+                                  relatedBy: .equal,
+                                  toItem: self,
+                                  attribute: attribute,
+                                  multiplier: 1,
+                                  constant: 0)
+        
+        scrollView.addConstraint(constraint)
     }
 
-    func setUpModels(models: [Models]) {
+    func addModelButtons() {
+        let height = scrollView.bounds.height
+        let width = scrollView.bounds.width/4
+        let frame = CGRect(x: 0, y: 0, width: width, height: height)
         
+        models.forEach { model in
+            let modelButton = UIButton(frame: frame)
+            modelButton.backgroundColor = .lightGray
+            modelButton.setTitle(model.fileName, for: .normal)
+            scrollView.addSubview(modelButton)
+        }
     }
 }
