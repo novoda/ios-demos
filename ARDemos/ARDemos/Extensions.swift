bde2ad35f6bff84f1d267862ee41bce98579c411
Wayne Rumble <rumblewayne@gmail.com>
2018-01-12 11:17:36 +0000

    Add float4x4 extension

---
+++ ARDemos/ARDemos/Extensions.swift
@@ -0,0 +1,8 @@
+import SceneKit
+
+extension float4x4 {
+    var translation: float3 {
+        let translation = self.columns.3
+        return float3(translation.x, translation.y, translation.z)
+    }
+}
