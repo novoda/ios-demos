dd181537aba30616626706c38c06e62c34bbbe6b
Wayne Rumble <rumblewayne@gmail.com>
2018-01-17 14:54:17 +0000

    Update custom colors

--- ARDemos/ARDemos/Helpers/Extensions.swift
+++ ARDemos/ARDemos/Helpers/Extensions.swift
@@ -8,22 +8,22 @@ extension float4x4 {
 }
 
 extension UIColor {
-        static let buttonText = UIColor(red: 1/255,
-                                        green: 1/255,
-                                        blue: 1/255,
-                                        alpha: 1.00)
-        static let selectedButton = UIColor(red: 128/255,
+        static let buttonText = UIColor(red: 0/255,
+                                        green: 0/255,
+                                        blue: 0/255,
+                                        alpha: 0.7)
+        static let selectedButton = UIColor(red: 216/255,
                                             green: 216/255,
-                                            blue: 223/255,
+                                            blue: 216/255,
                                             alpha: 1.00)
-        static let unSelectedButton = UIColor(red: 244/255,
-                                              green: 253/255,
-                                              blue: 174/255,
+        static let unSelectedButton = UIColor(red: 255/255,
+                                              green: 255/255,
+                                              blue: 255/255,
                                               alpha: 1.00)
-        static let bottomBar = UIColor(red: 54/255,
+        static let bottomBar = UIColor(red: 0/255,
                                        green: 0/255,
-                                       blue: 111/255,
-                                       alpha: 1.00)
+                                       blue: 0/255,
+                                       alpha: 0.5)
 }
 
 protocol Loopable {
