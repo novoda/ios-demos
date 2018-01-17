88ccc504e40a55115942ea60babc9bf73a520066
Wayne Rumble <rumblewayne@gmail.com>
2018-01-17 13:56:37 +0000

    Make Models equatable

--- ARDemos/ARDemos/Models/ObjectModels.swift
+++ ARDemos/ARDemos/Models/ObjectModels.swift
@@ -10,9 +10,28 @@ struct Model: Decodable {
     let planeSettings: PlaneSettings
 }
 
+extension Model {
+    static func ==(lhs: Model, rhs: Model) -> Bool {
+        
+        return lhs.filePath == rhs.filePath &&
+                lhs.fileName == rhs.fileName &&
+                lhs.fileExtension == rhs.fileExtension &&
+                lhs.nodes.count == rhs.nodes.count &&
+                lhs.lightSettings == rhs.lightSettings &&
+                lhs.planeSettings == rhs.planeSettings
+    }
+}
+
 struct Node: Decodable {
     let name: String
     let type: NodeType
+}
+
+extension Node  {
+    static func ==(lhs: Node, rhs: Node) -> Bool {
+        return lhs.name == rhs.name &&
+                lhs.type == rhs.type
+    }
 }
 
 enum NodeType: String, Decodable {
@@ -25,6 +44,14 @@ struct LightSetting: Decodable {
     let intensity: CGFloat
     let shadowMode: ShadowOptions
     let shadowSampleCount: Int
+}
+
+extension LightSetting  {
+    static func ==(lhs: LightSetting, rhs: LightSetting) -> Bool {
+        return lhs.intensity == rhs.intensity &&
+                lhs.shadowMode == rhs.shadowMode &&
+                lhs.shadowSampleCount == rhs.shadowSampleCount
+    }
 }
 
 enum ShadowOptions: String, Decodable {
@@ -48,12 +75,29 @@ struct PlaneSettings: Decodable {
     let colorBufferWriteMask: ColorBufferOptions
 }
 
+extension PlaneSettings  {
+    static func ==(lhs: PlaneSettings, rhs: PlaneSettings) -> Bool {
+        return lhs.writesToDepthBuffer == rhs.writesToDepthBuffer &&
+                lhs.colorBufferWriteMask == rhs.colorBufferWriteMask
+    }
+}
+
 struct ColorBufferOptions: Decodable, Loopable {
     let all: Bool
     let red: Bool
     let green: Bool
     let blue: Bool
     let alpha: Bool
+}
+
+extension ColorBufferOptions  {
+    static func ==(lhs: ColorBufferOptions, rhs: ColorBufferOptions) -> Bool {
+        return lhs.all == rhs.all &&
+                lhs.red == rhs.red &&
+                lhs.green == rhs.green &&
+                lhs.blue == rhs.blue &&
+                lhs.alpha == rhs.alpha
+    }
     
     func getOptionSet() -> SCNColorMask {
         var optionSet = SCNColorMask()
@@ -70,12 +114,12 @@ struct ColorBufferOptions: Decodable, Loopable {
     
     private func keyStringAsOption(_ propertyString: String) -> SCNColorMask? {
         switch propertyString {
-            case "all": return .all
-            case "red": return .red
-            case "green": return .green
-            case "blue": return .blue
-            case "alpha": return .alpha
-            default: return nil
+        case "all": return .all
+        case "red": return .red
+        case "green": return .green
+        case "blue": return .blue
+        case "alpha": return .alpha
+        default: return nil
         }
     }
 }
