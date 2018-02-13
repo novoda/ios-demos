724e2baa405ac62948f061d9bf582988e4eefb34
Berta Devant <berta@novoda.com>
2018-02-13 18:02:41 +0100

    small tweak for the buttons

--- ARExperiment/ARExperiment/StartViewController.swift
+++ ARExperiment/ARExperiment/StartViewController.swift
@@ -25,22 +25,27 @@ class StartViewController: UIViewController {
         oneModelUsingVectorsButton.backgroundColor = ButtonStyle.startButtonStyle.backgroundColor
         oneModelUsingVectorsButton.tintColor = ButtonStyle.startButtonStyle.textColor
         oneModelUsingVectorsButton.alpha = ButtonStyle.startButtonStyle.backgroundAlpha
+        oneModelUsingVectorsButton.titleLabel?.adjustsFontSizeToFitWidth = true
 
         oneModelUsingAnchorsButton.backgroundColor = ButtonStyle.startButtonStyle.backgroundColor
         oneModelUsingAnchorsButton.tintColor = ButtonStyle.startButtonStyle.textColor
         oneModelUsingAnchorsButton.alpha = ButtonStyle.startButtonStyle.backgroundAlpha
+        oneModelUsingAnchorsButton.titleLabel?.adjustsFontSizeToFitWidth = true
 
         sizeComparisonButton.backgroundColor = ButtonStyle.startButtonStyle.backgroundColor
         sizeComparisonButton.tintColor = ButtonStyle.startButtonStyle.textColor
         sizeComparisonButton.alpha = ButtonStyle.startButtonStyle.backgroundAlpha
+        sizeComparisonButton.titleLabel?.adjustsFontSizeToFitWidth = true
 
         recognizeObjectsButton.backgroundColor = ButtonStyle.startButtonStyle.backgroundColor
         recognizeObjectsButton.tintColor = ButtonStyle.startButtonStyle.textColor
         recognizeObjectsButton.alpha = ButtonStyle.startButtonStyle.backgroundAlpha
+        recognizeObjectsButton.titleLabel?.adjustsFontSizeToFitWidth = true
 
         lightsAnimationButton.backgroundColor = ButtonStyle.startButtonStyle.backgroundColor
         lightsAnimationButton.tintColor = ButtonStyle.startButtonStyle.textColor
         lightsAnimationButton.alpha = ButtonStyle.startButtonStyle.backgroundAlpha
+        lightsAnimationButton.titleLabel?.adjustsFontSizeToFitWidth = true
     }
 
 }
@@ -55,7 +60,7 @@ private extension ButtonStyle {
     static var startButtonStyle = ButtonStyle(
         backgroundColor: .blue,
         textColor: .white,
-        backgroundAlpha: 0.65)
+        backgroundAlpha: 0.35)
 }
 
 private extension UIViewController {
