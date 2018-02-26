158bd2a96b191f72f292193de8bb7c4f53d998f1
Bart Ziemba <bart@novoda.com>
2018-02-26 14:41:54 +0000

    Remove unused function.

--- Cucumberish-demo/MiraclePillUITests/StepsDefinitions/CommonSteps.swift
+++ Cucumberish-demo/MiraclePillUITests/StepsDefinitions/CommonSteps.swift
@@ -9,9 +9,5 @@ class CommonStepDefinitions: Common {
         Given("user launches the application") { _,_ in
             self.launchApplication()
         }
-        
-        Given("asaas") { _,_ in
-            
-        }
     }
 }
