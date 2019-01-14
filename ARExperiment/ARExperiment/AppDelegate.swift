4d67c784dd96a6cb2dfded1656cdfd91dc74c70a
Berta Devant <berta@novoda.com>
2019-01-14 18:44:41 +0100

    eliminate useless commnents

--- ARExperiment/ARExperiment/AppDelegate.swift
+++ ARExperiment/ARExperiment/AppDelegate.swift
@@ -1,11 +1,3 @@
-//
-//  AppDelegate.swift
-//  ARExperiment
-//
-//  Created by Berta Devant on 02/01/2018.
-//  Copyright © 2018 Berta Devant. All rights reserved.
-//
-
 import UIKit
 
 @UIApplicationMain
@@ -14,14 +6,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
-        // Sets background to a blank/empty image
         UINavigationBar.appearance().setBackgroundImage(UIImage(), for: .default)
-        // Sets shadow (line below the bar) to a blank image
         UINavigationBar.appearance().shadowImage = UIImage()
-        // Sets the translucent background color
         UINavigationBar.appearance().backgroundColor = .clear
-        // Set translucent. (Default value is already true, so this can be removed if desired.)
         UINavigationBar.appearance().isTranslucent = true
         return true
     }
