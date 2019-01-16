f3a445f16c82beaeaffbd598b521919b1f4c997a
Berta Devant <berta@novoda.com>
2019-01-16 17:36:14 +0100

    add solid view controller

---
+++ ARImageTrackingExamples/ARImageTrackingExamples/View Controllers/SolidCardViewController.swift
@@ -0,0 +1,20 @@
+import Foundation
+import UIKit
+import ARKit
+
+class SolidCardViewController: UIViewController, ARSCNViewDelegate {
+
+    @IBOutlet var sceneView: ARSCNView!
+
+    override func viewDidLoad() {
+
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+
+    }
+
+    override func viewWillDisappear(_ animated: Bool) {
+
+    }
+}
