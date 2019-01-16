a923978afd1223507e71002e543f274b36299ce4
Berta Devant <berta@novoda.com>
2019-01-16 18:39:31 +0100

    add image tracking to the solid cards controller

--- ARImageTrackingExamples/ARImageTrackingExamples/View Controllers/SolidCardViewController.swift
+++ ARImageTrackingExamples/ARImageTrackingExamples/View Controllers/SolidCardViewController.swift
@@ -2,19 +2,43 @@ import Foundation
 import UIKit
 import ARKit
 
-class SolidCardViewController: UIViewController, ARSCNViewDelegate {
+class SolidCardViewController: UIViewController, ARExperimentSessionHandler {
 
     @IBOutlet var sceneView: ARSCNView!
+    private let arSessionDelegate = ARExperimentSession()
+    private let arViewModel = ARViewModel()
 
     override func viewDidLoad() {
-
+        sceneView.delegate = self
+        arSessionDelegate.sessionHandler = self
+        sceneView.session.delegate = arSessionDelegate
     }
 
     override func viewWillAppear(_ animated: Bool) {
 
+        let configuration = arViewModel.imageTrackingConfiguration()
+        sceneView.session.run(configuration, options: [.removeExistingAnchors, .resetTracking])
     }
 
     override func viewWillDisappear(_ animated: Bool) {
-
+        sceneView.session.pause()
     }
 }
+
+extension SolidCardViewController: ARSCNViewDelegate {
+
+    func renderer(_ renderer: SCNSceneRenderer, didAdd node: SCNNode, for anchor: ARAnchor) {
+        guard let imageAnchor = anchor as? ARImageAnchor else {
+            return
+        }
+
+        let plane = SCNPlane(width: imageAnchor.imageSize().width,
+                             height: imageAnchor.imageSize().height)
+        plane.styleFirstMaterial(with: UIColor.blue)
+        let planeNode = SCNNode(geometry: plane)
+        planeNode.opacity = 0.55
+        planeNode.eulerAngles.x = -.pi / 2
+        node.addChildNode(planeNode)
+    }
+}
+
