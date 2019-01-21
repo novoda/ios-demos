6eba4bdc5eec196220775b4ae0cb4847a4ac3263
Berta Devant <berta@novoda.com>
2019-01-21 12:38:04 +0100

    use the new recognize objects model on vc and remove un necessary functions and variables that are on model now

--- ARExperiment/ARExperiment/RecognizeObjectsViewController.swift
+++ ARExperiment/ARExperiment/RecognizeObjectsViewController.swift
@@ -6,16 +6,12 @@ import Vision
 class RecognizeObjectsViewController: UIViewController {
     
     @IBOutlet weak var sceneView: ARSCNView!
-    fileprivate let yolo = YOLO()
-    private let semaphore = DispatchSemaphore(value: 2)
-    private var request: VNCoreMLRequest!
     private let startButton = UIButton()
     private let arAsset = ARAsset.cubeWireframe
     private var arViewModel: ARViewModel!
-    private var currentSceneFrame: CGRect = .zero
     private let arSessionDelegate = ARExperimentSession()
     private var nodesForSession: [SCNNode]?
-    private var prediction: ObjectPrediction?
+    private let recognizeObjectsViewModel = RecognizeObjectsViewModel()
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -28,7 +24,7 @@ class RecognizeObjectsViewController: UIViewController {
         sceneView.debugOptions = [ARSCNDebugOptions.showFeaturePoints]
 
         setupStartButton()
-        setUpVision()
+        recognizeObjectsViewModel.setUpVision()
         view.backgroundColor = .white
         styleNavigationBar(with: .white)
     }
@@ -39,7 +35,7 @@ class RecognizeObjectsViewController: UIViewController {
         let configuration = ARWorldTrackingConfiguration()
         configuration.planeDetection = .horizontal
         sceneView.session.run(configuration)
-        currentSceneFrame = sceneView.frame
+        recognizeObjectsViewModel.setCurrentFrameForModel(sceneView.frame)
     }
     
     override func viewWillDisappear(_ animated: Bool) {
@@ -65,9 +61,8 @@ class RecognizeObjectsViewController: UIViewController {
     @objc private func startButtonHasBeenPressed(_ sender: UIButton) {
         guard let pixelBuffer = sceneView.session.currentFrame?.capturedImage else { return }
         startButton.isHidden = true
-        semaphore.wait()
         DispatchQueue.global().async { [weak self] in
-            self?.predictUsingVision(pixelBuffer: pixelBuffer)
+            self?.recognizeObjectsViewModel.predictUsingVision(pixelBuffer: pixelBuffer)
         }
     }
 
@@ -75,100 +70,6 @@ class RecognizeObjectsViewController: UIViewController {
         DispatchQueue.main.async { [weak self] in
             self?.startButton.isHidden = false
         }
-    }
-    
-    //MARK: Vision Prediction
-    private func predictUsingVision(pixelBuffer: CVPixelBuffer) {
-        // Vision will automatically resize the input image.
-        let handler = VNImageRequestHandler(cvPixelBuffer: pixelBuffer)
-        try? handler.perform([request])
-    }
-    
-    private func setUpVision() {
-        if usingTinyModel {
-            guard let visionModel = try? VNCoreMLModel(for: yolo.model.model) else {
-                print("Error: could not create Vision model")
-                return
-            }
-            request = VNCoreMLRequest(model: visionModel, completionHandler: visionRequestDidComplete)
-        } else {
-            guard #available(iOS 12.0, *) else {
-                usingTinyModel = true
-                setUpVision()
-                return
-            }
-            guard let visionModel = try? VNCoreMLModel(for: ObjectDetector().model) else {
-                print("Error: could not create Vision model")
-                return
-            }
-            request = VNCoreMLRequest(model: visionModel, completionHandler: visionRequestDidComplete)
-        }
-
-        // NOTE: If you choose another crop/scale option, then you must also
-        // change how the BoundingBox objects get scaled when they are drawn.
-        // Currently they assume the full input image is used.
-        request.imageCropAndScaleOption = .scaleFill
-    }
-    
-    func visionRequestDidComplete(request: VNRequest, error: Error?) {
-        self.semaphore.signal()
-
-        if usingTinyModel {
-            prediction = processPredictionFromTinyYolo(results: request.results)
-        } else {
-            prediction = processPredictionFromVision(results: request.results)
-        }
-        addBoxOnMainThread(prediction)
-    }
-
-    private func processPredictionFromTinyYolo(results: [Any]?) -> ObjectPrediction? {
-        guard let observations = results as? [VNCoreMLFeatureValueObservation],
-            let topObservation = observations.first?.featureValue.multiArrayValue else {
-                print("Tiny YOLO failed finding prediction" )
-                showStartButtonIfError()
-                return nil
-        }
-
-        guard let prediction = predictionForTinyYOLO(topObservation: topObservation),
-            let objectBounds = boundingBoxRectForTinyYOLO(prediction: prediction) else {
-                showStartButtonIfError()
-                print("Tiny YOLO failed finding top prediction" )
-                return nil
-        }
-        return ObjectPrediction(name: objectNameForTinyYOLO(prediction: prediction), bounds: objectBounds)
-    }
-
-    private func processPredictionFromVision(results: [Any]?) -> ObjectPrediction? {
-        guard #available(iOS 12.0, *) else {
-            usingTinyModel = true
-            return nil
-        }
-        guard let observations = results as? [VNRecognizedObjectObservation],
-            let topObservation = observations.first,
-            let topLabelObservation = topObservation.labels.first else {
-                print("Vision failed finding prediction" )
-                showStartButtonIfError()
-                return nil
-        }
-        let objectRect = VNImageRectForNormalizedRect(topObservation.boundingBox,
-                                                      Int(currentSceneFrame.width),
-                                                      Int(currentSceneFrame.height))
-        return ObjectPrediction(name: topLabelObservation.identifier, bounds: objectRect)
-    }
-
-    private func predictionForTinyYOLO(topObservation: MLMultiArray) -> YOLO.Prediction? {
-        let boundingBoxes = yolo.computeBoundingBoxes(features: topObservation)
-        let prominentBox = boundingBoxes.sorted{ $0.score > $1.score}
-        return prominentBox.first
-    }
-
-    private func boundingBoxRectForTinyYOLO(prediction: YOLO.Prediction) -> CGRect? {
-        let viewRect = CGRect(x: 0, y: 0, width: Int(currentSceneFrame.width), height: Int(currentSceneFrame.height))
-        return yolo.scaleImageForCameraOutput(predictionRect: prediction.rect, viewRect: viewRect)
-    }
-
-    private func objectNameForTinyYOLO(prediction: YOLO.Prediction) -> String {
-        return labels[prediction.classIndex]
     }
 
     //MARK: Add Model to the scene
