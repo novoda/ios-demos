b390b97af277754d0b95084109e073c368ffd1b0
Berta Devant <berta@novoda.com>
2019-01-25 11:09:12 +0100

    create a new imageTracking configuration

--- ARImageTrackingExamples/ARImageTrackingExamples/ViewModels/ARViewModel.swift
+++ ARImageTrackingExamples/ARImageTrackingExamples/ViewModels/ARViewModel.swift
@@ -8,6 +8,16 @@ class ARViewModel {
         guard let referenceImages = ARReferenceImage.referenceImages(inGroupNamed: "AR Resources", bundle: nil) else {
             fatalError("Missing expected asset catalog resources.")
         }
+        let configuration = ARImageTrackingConfiguration()
+        configuration.trackingImages = referenceImages
+        configuration.maximumNumberOfTrackedImages = 2
+        return configuration
+    }
+
+    func worldTrackingWithImageDetection() -> ARConfiguration {
+        guard let referenceImages = ARReferenceImage.referenceImages(inGroupNamed: "AR Resources", bundle: nil) else {
+            fatalError("Missing expected asset catalog resources.")
+        }
         let configuration = ARWorldTrackingConfiguration()
         configuration.detectionImages = referenceImages
         configuration.maximumNumberOfTrackedImages = 2
