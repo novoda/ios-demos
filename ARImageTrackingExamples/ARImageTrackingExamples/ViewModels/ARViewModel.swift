bc88df969a63df9fd2c843fce6fcc45a8760f80b
Berta Devant <berta@novoda.com>
2019-01-28 18:28:51 +0100

    add view model functions to calculate animations and variables

--- ARImageTrackingExamples/ARImageTrackingExamples/ViewModels/ARViewModel.swift
+++ ARImageTrackingExamples/ARImageTrackingExamples/ViewModels/ARViewModel.swift
@@ -34,8 +34,8 @@ class ARViewModel {
     func image(correspondingTo referenceImage: ARReferenceImage) -> UIImage? {
         guard let referenceImageName = referenceImage.name,
             let correspondingImageName = assets[referenceImageName] else {
-            print("no image found")
-            return nil
+                print("no image found")
+                return nil
         }
         return correspondingImageName.image()
     }
@@ -53,5 +53,56 @@ class ARViewModel {
         }
         let node = scene.rootNode.childNode(withName: nodeName, recursively: true)
         return node
+    }
+
+    func animationVariable(updateAtTime time: TimeInterval,
+                           referenceImageNode: SCNNode,
+                           referenenceNode: SCNNode) -> AnimationInfo {
+        return refreshAnimationVariables(startTime: time,
+                                         initialPosition: referenenceNode.simdWorldPosition,
+                                         finalPosition: referenceImageNode.simdWorldPosition,
+                                         initialOrientation: referenenceNode.simdWorldOrientation,
+                                         finalOrientation: referenceImageNode.simdWorldOrientation)
+    }
+
+    func positionRelativeTo(animationInfo: AnimationInfo, using time: TimeInterval) -> simd_float3 {
+        // 3. Calculate interpolation based on passedTime/totalTime ratio.
+        let passedTime = time - animationInfo.startTime
+        let minTime = min(Float(passedTime/animationInfo.duration), 1)
+        // Calculate "ease out" timing by curving function to time parameter
+        let easeOutTime = sin(minTime * .pi * 0.5)
+
+        // 4. Calculate and set new model position and orientation.
+        let positionForEaseOutTime = simd_make_float3(easeOutTime, easeOutTime, easeOutTime)
+        return simd_mix(animationInfo.initialModelPosition,
+                        animationInfo.finalModelPosition,
+                        positionForEaseOutTime)
+    }
+
+    func orientationRelativeTo(animationInfo: AnimationInfo, using time: TimeInterval) -> simd_quatf {
+        // 3. Calculate interpolation based on passedTime/totalTime ratio.
+        let passedTime = time - animationInfo.startTime
+        let minTime = min(Float(passedTime/animationInfo.duration), 1)
+        // Calculate "ease out" timing by curving function to time parameter
+        let easeOutTime = sin(minTime * .pi * 0.5)
+        return simd_slerp(animationInfo.initialModelRotation, animationInfo.finalModelRotation, easeOutTime)
+    }
+
+    private func refreshAnimationVariables(startTime: TimeInterval,
+                                   initialPosition: float3,
+                                   finalPosition: float3,
+                                   initialOrientation: simd_quatf,
+                                   finalOrientation: simd_quatf) -> AnimationInfo {
+        let distance = simd_distance(initialPosition, finalPosition)
+        // Average speed of movement is 0.15 m/s.
+        let speed = Float(0.15)
+        // Total time is calculated as distance/speed. Min time is set to 0.1s and max is set to 2s.
+        let animationDuration = Double(min(max(0.1, distance/speed), 2))
+        return AnimationInfo(startTime: startTime,
+                             duration: animationDuration,
+                             initialModelPosition: initialPosition,
+                             initialModelRotation: initialOrientation,
+                             finalModelPosition: finalPosition,
+                             finalModelRotation: finalOrientation)
     }
 }
