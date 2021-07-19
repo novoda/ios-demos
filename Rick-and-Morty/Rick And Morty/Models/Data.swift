01a0fd3c587a28a0985ab32cdcc238862da87140
Scottie Gray <sgray@Scotties-Air.home>
2021-07-19 10:43:36 +0100

    Created custom TabViewController and created generic Character views and viewControllers

--- Rick-and-Morty/Rick And Morty/Models/Data.swift
+++ Rick-and-Morty/Rick And Morty/Models/Data.swift
@@ -1,4 +1,13 @@
-import UIKit
+import Foundation
+
+let ricks = [
+    Rick(name: "Rick Prime", image: "Rick-2", description: "Rick Prime's appearance is almost identical to the other members of the Council of Ricks. The only difference is that his hair style consists of a large puff of hair on top, with the sides and back of his head shaved."),
+    Rick(name: "Quantum Rick", image: "Rick-3", description: "Quantum Rick shares the general appearance of the titular Rick Sanchez, but sports a puffier hairstyle."),
+    Rick(name: "Maximums Rickimus", image: "Rick-4", description: "Maximums Rickimus shares the general appearance of Rick Sanchez, though with a different hairstyle."),
+    Rick(name: "Riq IV", image: "Rick-5", description: "Riq IV shares the general appearance of Rick Sanchez, though with a much different hairstyle, consisting of 3 spikes, and a long soul patch."),
+    Rick(name: "Zeta Alpha Rick", image: "Rick-6", description: "Zeta Alpha Rick shares the general appearance of Rick Sanchez, though with a different hairstyle."),
+    Rick(name: "Ricktiminus Sancheziminius", image: "Rick-7", description: "Ricktiminus Sancheziminius shares the general appearance of Rick Sanchez, though with a different hairstyle, and facial hair."),
+]
 
 let morties = [
     Morty(name: "Morty", image: "PM-1", shortDescription: "This is your Morty, don't lose him!", description: "Morty is the Morty you start out with in Pocket Mortys. Along with Cronenberg Morty and Morticia, he is different from all the other Mortys because he has no type (Rock, paper or scissors) and has no great advantage/disadvantage over any type. He is also the only Morty that cannot be released."),
@@ -9,31 +18,3 @@ let morties = [
     Morty(name: "Ad Space Morty", image: "PM-22", shortDescription: "This Morty is working for free for Jerry. He hopes to never see another apple in his life.", description: "Ad Space Morty appears in the game Pocket Mortys. He evolves into Jerry's Game Morty, which in turn evolves into Pocket Mortys Morty."),
     Morty(name: "Blue Shirt Morty", image: "PM-25", shortDescription: "This Morty does not want to be defined by wearing the same yellow shirt every day, as other Mortys do. He opted for blue, the color of the summer sky and the distant seas.", description: "Blue Shirt Morty appears in the game Pocket Mortys.")
 ]
-
-class MortyCollectionViewController: UICollectionViewController {
-    override func viewDidLoad() {
-        self.title = "Morty"
-
-        if let flowLayout = collectionViewLayout as? UICollectionViewFlowLayout {
-            flowLayout.itemSize = CGSize(width: 320, height: 120)
-        }
-    }
-
-    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return morties.count
-    }
-
-    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "morty-cell-identifier", for:indexPath)
-        guard let mortyCell = cell as? MortyCollectionViewCell else {
-            return cell
-        }
-
-        let morty = morties[indexPath.row]
-        mortyCell.imageView.image = UIImage(named: morty.image)
-        mortyCell.nameLabel.text = morty.name
-        mortyCell.descriptionLabel.text = morty.description
-
-        return mortyCell
-    }
-}
--- Rick-and-Morty/Rick And Morty/RickCollectionViewCell.swift
+++
@@ -1,7 +0,0 @@
-import UIKit
-
-class RickCollectionViewCell: UICollectionViewCell {
-    @IBOutlet weak var imageView: UIImageView!
-    @IBOutlet weak var nameLabel: UILabel!
-    @IBOutlet weak var descriptionLabel: UILabel!
-}
