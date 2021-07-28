3c4e326e8f7c8a395adcb8e6418ae728fa3b9872
swg99 <87419041+swg99@users.noreply.github.com>
2021-07-28 11:48:40 +0100

    Added CharactersView

---
+++ Rick-and-Morty/Rick And Morty/Views/CharactersView.swift
@@ -0,0 +1,13 @@
+import SwiftUI
+
+struct CharactersView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CharactersView_Previews: PreviewProvider {
+    static var previews: some View {
+        CharactersView()
+    }
+}
