746e5efa967e3e420f5a7eba909bbda6d745732c
swg99 <87419041+swg99@users.noreply.github.com>
2021-08-13 11:39:51 +0100

    Built episode repository
    Provides an episode for an episode url

---
+++ Rick-and-Morty/EpisodeRepository.swift
@@ -0,0 +1,34 @@
+//
+//  EpisodeRepository.swift
+//  Rick And Morty
+//
+//  Created by Scottie Gray on 2021-08-13.
+//  Copyright © 2021 Novoda. All rights reserved.
+//
+
+import Foundation
+
+protocol EpisodeRepositoryProtocol {
+    func getEpisode(from urlString: String, completion: @escaping ((Episode) -> Void))
+}
+
+final class EpisodeRepository: EpisodeRepositoryProtocol {
+    private let rickAndMortyService: RickAndMortyServiceProtocol = RickAndMortyService()
+    private var cachedEpisodeNames: [String : Episode] = [:]
+
+    func getEpisode(from urlString: String, completion: @escaping ((Episode) -> Void)) {
+        if let episode = cachedEpisodeNames[urlString] {
+            completion(episode)
+        } else {
+            if let url = URL(string: urlString) {
+                rickAndMortyService.fetchData(url: url) { (episode: Episode) in
+                    self.cachedEpisodeNames[urlString] = episode
+                    completion(episode)
+                } error: { error in
+                    print(error.debugDescription)
+                    return
+                }
+            }
+        }
+    }
+}
---
+++ Rick-and-Morty/Rick And Morty/Model/Episode.swift
@@ -0,0 +1,13 @@
+//
+//  Episode.swift
+//  Rick And Morty
+//
+//  Created by Scottie Gray on 2021-08-13.
+//  Copyright © 2021 Novoda. All rights reserved.
+//
+
+import Foundation
+
+struct Episode: Codable {
+    let name: String
+}
